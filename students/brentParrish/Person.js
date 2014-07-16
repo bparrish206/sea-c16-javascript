bff8785210d18e10ccfedeafb60b3491458f1223
bparrish206 <brentparrish76@gmail.com>
2014-07-16 16:34:00 -0700

    updated person func

--- students/brentParrish/Person.js
+++ students/brentParrish/Person.js
@@ -1,9 +1,17 @@
-function Person(name, age, parentName){
-	this.name = name;
-	this.age = age;
-	this.parentName = parentName;
-	this.isDead = function(age) {
-		if (!age) {
-			return "Is dead"
-		}
-}
+var Person = function(name, parentName, age) { 
+	this.name = name; 
+	this.parentName = parentName; 
+	this.age = age; 
+	this.isAlive = function() { 
+		if (age > 0 && age <= 150) { 
+ 			return "Is alive" 
+ 			} 
+		else { return "Is dead"} 
+  			} 
+ 	this.isDead = function() { 
+  		if (!age) { 
+ 			return "Is dead" 
+ 		} 
+  		else {return this.isAlive()} 
+  		} 
+
